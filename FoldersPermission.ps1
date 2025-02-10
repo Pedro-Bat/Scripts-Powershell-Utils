8cee4d90fd4f9b100cebeee542d6fe62da68a8c4
Pedro-Bat <104031353+Pedro-Bat@users.noreply.github.com>
2025-02-10 13:17:39 -0300

    Update FoldersPermission.ps1

--- FoldersPermission.ps1
+++ FoldersPermission.ps1
@@ -1,10 +1,10 @@
 #Script que le todas as pastas de um determinado diretorio e audita usuários e suas permissões sendo usuários locais ou de dominio e deixa num json.
-
+#Os parametros -directory e -depth  podem não funcionar se o script estiver sendo executado como administrador
 
 $diretorio = "caminho do diretorio"
 $resultados = @{}
 
-$pastas = Get-ChildItem -Path $diretorio -Directory #-recurse
+$pastas = Get-ChildItem -Path $diretorio -Directory #-recurse -depth <int>
 
 foreach ($pasta in $pastas) {
     $acls = Get-Acl -Path $pasta.FullName | Select-Object -ExpandProperty Access
@@ -15,17 +15,31 @@ foreach ($pasta in $pastas) {
             $grupo = $acl.IdentityReference.Value.Split('\')[1]
             try{
             $membros = Get-ADGroupMember -Identity $grupo -Recursive | Where-Object { $_.objectClass -eq 'user' } | Select-Object -ExpandProperty Name
-               }catch{$grupo = $null}
-               
-            foreach ($membro in $membros) {
-                if (-not $resultados.ContainsKey($pasta.FullName)) {
+               }catch{
+                   if (-not $resultados.ContainsKey($pasta.FullName)) {
                     $resultados[$pasta.FullName] = @()
+                   }
+
+                   $resultados[$pasta.FullName] += @{
+                    Permissões = $acl.FileSystemRights.ToString()
+                    Usuário = $grupo
+                    Grupo = $null
+                    }
+                    
+                    $membros = 'Null'
                 }
 
-                $resultados[$pasta.FullName] += @{
-                    Permissões = $acl.FileSystemRights.ToString()
-                    Usuário = $membro
-                    Grupo = $grupo
+            if($membros -ne 'Null'){   
+                foreach ($membro in $membros) {
+                    if (-not $resultados.ContainsKey($pasta.FullName)) {
+                        $resultados[$pasta.FullName] = @()
+                    }
+
+                    $resultados[$pasta.FullName] += @{
+                        Permissões = $acl.FileSystemRights.ToString()
+                        Usuário = $membro
+                        Grupo = $grupo
+                    } 
                 }
             }
         }
@@ -34,4 +48,4 @@ foreach ($pasta in $pastas) {
 
 $json = $resultados | ConvertTo-Json
 
-$json | Out-File -FilePath 'caminho do output' -Encoding utf8
+$json | Out-File -FilePath 'caminho do output.permissions.json' -Encoding utf8
